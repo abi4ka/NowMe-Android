ad3ed76e1d724e0f7d355814184304670329414f
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2026-03-13 12:00:24 +0100

    Create LoginActivity

---
+++ NowMe/app/src/main/java/com/example/nowme/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.nowme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    TextView tvSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignup = findViewById(R.id.tvSignup);
+
+        btnLogin.setOnClickListener(v -> login());
+    }
+
+    private void login() {
+
+        String user = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+
+        //TODO: connect backend
+        if (user.equals("test") && password.equals("1234")) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}

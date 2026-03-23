73fe18e9a06a8672b36b1658d026b3da1230d474
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2026-03-23 10:51:01 +0100

    Connecting buttons

--- NowMe/app/src/main/java/com/example/nowme/NowmeActivity.java
+++ NowMe/app/src/main/java/com/example/nowme/NowmeActivity.java
@@ -1,14 +1,21 @@
 package com.example.nowme;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class NowmeActivity extends AppCompatActivity {
+
+    ImageButton btnClose, btnMenu, btnLike, btnComment;
+    TextView tvNumLike, tvNumComment;
+    int likes;
+    boolean liked = false;
+    boolean isOwner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,52 @@ public class NowmeActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nowme);
 
+        btnClose = findViewById(R.id.btnClose);
+        btnMenu = findViewById(R.id.btnMenu);
+        btnLike = findViewById(R.id.btnLike);
+        btnComment = findViewById(R.id.btnComment);
+        tvNumLike = findViewById(R.id.tvNumLike);
+        tvNumComment = findViewById(R.id.tvNumComment);
+
+        likes = getIntent().getIntExtra("likes", 0);
+        liked = getIntent().getBooleanExtra("liked", false);
+        isOwner = getIntent().getBooleanExtra("isOwner", false);
+
+        btnClose.setOnClickListener(v -> finish());
+
+        btnMenu.setOnClickListener(v -> {
+            Toast.makeText(this, "Menú (delete / pin)", Toast.LENGTH_SHORT).show();
+        });
+
+        if (isOwner) {
+            tvNumLike.setVisibility(TextView.VISIBLE);
+            tvNumLike.setText(String.valueOf(likes));
+        } else {
+            tvNumLike.setVisibility(TextView.GONE);
+        }
+
+        btnLike.setOnClickListener(v -> {
+
+            liked = !liked;
+
+            btnLike.setImageResource(liked ? R.drawable.ic_heart : R.drawable.ic_heart_empty);
+
+            int likes = Integer.parseInt(tvNumLike.getText().toString());
+
+            if (liked) {
+                likes++;
+            } else {
+                likes--;
+            }
+
+            tvNumLike.setText(String.valueOf(likes));
+        });
+
+        btnComment.setOnClickListener(v -> {
+            Toast.makeText(this, "Abrir comentarios", Toast.LENGTH_SHORT).show();
+
+            // abrir teclado
+            //hacer scroll al RecyclerView
+        });
     }
 }

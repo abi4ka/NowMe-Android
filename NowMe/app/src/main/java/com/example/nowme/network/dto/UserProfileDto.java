29f4315b0d4ca030c7e926d4bdf7162aca876e09
Gabriela <gvazquezi24@fpcoruna.afundacion.org>
2026-04-20 22:50:55 +0200

    Create UserProfileDto.java

---
+++ NowMe/app/src/main/java/com/example/nowme/network/dto/UserProfileDto.java
@@ -0,0 +1,14 @@
+package com.example.nowme.network.dto;
+
+public class UserProfileDto {
+    public Long id;
+    public String username;
+    public String avatar;
+    public Long followers;
+    public Long following;
+    public Long friends;
+
+    public boolean me;
+    public boolean followingUser;
+    public boolean friend;
+}
